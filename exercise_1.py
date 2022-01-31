942e10430d482e048227d57b41f3832c186b8a8a
JustinArnold-tech <79416942+JustinArnold-tech@users.noreply.github.com>
2022-01-31 12:24:59 -0500

    exercise_1 bug fix

--- exercise_1.py
+++ exercise_1.py
@@ -1,16 +1,22 @@
+class Counts:
+    def stringCount(self,str1):
+        alphabets = 0
+        digits = 0
+        special = 0
+
+        for i in range(len(str1)):
+            if((str1[i] >= 'a' and str1[i] <= 'z') or (str1[i] >= 'A' and str1[i] <= 'Z')):
+                alphabets = alphabets + 1
+            elif(str1[i] >= '0' and str1[i] <= '9'):
+                digits = digits + 1
+            else:
+                special = special + 1
+        return alphabets, digits, special
 str1 = "P@#yn26at^&i5ve"
-alphabets = 0
-digits = 0
-special = 0
+# print("Chars:  ", alphabets)
+# print("Digits:  ", digits)
+# print("Symbol:  ", special)
 
-for i in range(len(str1)):
-    if((str1[i] >= 'a' and str1[i] <= 'z') or (str1[i] >= 'A' and str1[i] <= 'Z')):
-        alphabets = alphabets + 1
-    elif(str1[i] >= '0' and str1[i] <= '9'):
-        digits = digits + 1
-    else:
-        special = special + 1
-
-print("Chars:  ", alphabets)
-print("Digits:  ", digits)
-print("Symbol:  ", special)
+if __name__ == '__main__':
+  func1 = Counts()
+  print(func1.stringCount(str1))
