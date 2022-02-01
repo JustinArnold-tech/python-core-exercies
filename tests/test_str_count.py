191e28114b6e6a3b390877ab1541174d010cb5f4
JustinArnold-tech <79416942+JustinArnold-tech@users.noreply.github.com>
2022-02-01 10:36:50 -0500

    Update test_str_count.py

--- tests/test_str_count.py
+++ tests/test_str_count.py
@@ -7,8 +7,7 @@ class StrCountTest(unittest.TestCase):
         cls.count = Counts()
     
     def test_count_chars(self):
-        str1 = "P@#yn26at^&i5ve"
-        self.assertEqual(self.count.stringCount(str1), (8, 3, 4))
+        self.assertEqual(self.count.stringCount("P@#yn26at^&i5ve"), (8, 3, 4))
 
 if __name__ == '__main__':
     unittest.main()
